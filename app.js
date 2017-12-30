663ec969a6052d1cb2edab8894815b72014d8a13
yangdage <515411030@qq.com>
2017-12-30 15:28:03 +0800

    handle 跨域

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ var port = config.port || 3000;
 var routes = config.routes || [];
 var defaultMethod = (config.defaultMethod || 'get').toLowerCase();
 
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
 for(var i = 0; i < routes.length; i++){
     (function (n) {
         var router = routes[n],
             method = (router.method || defaultMethod).toLowerCase();
         app[method](router.path, function (req, res) {
-            res.json({
-                status: 200,
-                data: router.data
-            });
+            res.json(router.data);
         });
     })(i)
 }
